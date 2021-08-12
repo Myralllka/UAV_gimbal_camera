8441f6748241d4042f947102b3d6bf22c5ce72e6
Myralllka <Olli25010@gmail.com>
2021-08-12 15:50:41 +0200

    cleaned the code

--- include/GimbalCameraNodelet.h
+++ include/GimbalCameraNodelet.h
@@ -31,8 +31,6 @@ T deg2rad(const T x) { return x * M_PI / 180; }
 template<typename T>
 T rad2deg(const T x) { return x / M_PI * 180; }
 
-//}
-
 namespace gimbal_camera {
 
 /* class GimbalCameraNodelet //{ */
@@ -46,54 +44,36 @@ namespace gimbal_camera {
         /* flags */
         bool m_recv_camera_info = false;
         /* ros parameters */
+
+        float m_max_angle;
+        float m_time_before_centering;
+        float m_max_x_error;
+        float m_max_y_error;
+        std::string m_uav_name;
+
+        ros::Timer m_timer_centering;
         sensor_msgs::CameraInfo m_camera_info;
 
-        float m_yaw_movement{0.0};
-        float m_pitch_movement{0.0};
-        const float m_step{0.001};
-        const float m_max_angle{0.28};
-        const float m_time_before_centering{1};
-
-        const float m_max_x_error{0.005};
-        const float m_max_y_error{0.005};
-        ros::Timer m_timer_centering;
-
-        std::mutex m_movement_mutex;
         // | --------------------- MRS transformer -------------------- |
         mrs_lib::Transformer m_transformer;
 
         // | ---------------------- msg callbacks --------------------- |
 
         void m_cbk_camera_info(const sensor_msgs::CameraInfo::ConstPtr &msg);
-
         void m_cbk_tag_detection(const apriltag_ros::AprilTagDetectionArray msg);
-        // | --------------------- timer callbacks -------------------- |
-
-        // | --------- variables, related to message checking --------- |
-
 
         // | ----------------------- publishers ----------------------- |
-
         ros::Publisher m_pub_transform2gimbal_pry;
         ros::Publisher m_pub_transform2gimbal_quat;
 
         // | ----------------------- subscribers ---------------------- |
-
         ros::Subscriber m_sub_camera_info;
         ros::Subscriber m_sub_tag_detection;
-//        ros::Subscriber m_sub_gimbal_;
+
         // | --------------------- other functions -------------------- |
-
-        void follow_apriltag_incremental(const ros::TimerEvent &ev);
-
-        void follow_apriltag_from_two_vectors();
 
         void center_camera(const ros::TimerEvent &ev);
 
-        [[maybe_unused]] inline float calculate_step(const float current_error, const float max_error) const {
-            float step = std::abs(current_error) / max_error * m_step;
-            return step;
-        }
     };
 //}
 
--- src/GimbalCameraNodelet.cpp
+++ src/GimbalCameraNodelet.cpp
@@ -7,8 +7,6 @@ namespace gimbal_camera {
 /* onInit() method //{ */
     void GimbalCameraNodelet::onInit() {
 
-        // | ---------------- set my booleans to false ---------------- |
-
         /* obtain node handle */
         ros::NodeHandle nh = nodelet::Nodelet::getMTPrivateNodeHandle();
 
@@ -16,23 +14,31 @@ namespace gimbal_camera {
         ros::Time::waitForValid();
 
         // | ------------------- load ros parameters ------------------ |
+        mrs_lib::ParamLoader pl(nh);
+        pl.loadParam2("uav_name", m_uav_name);
+        pl.loadParam("max_angle", m_max_angle);
+        pl.loadParam("time_before_centering", m_time_before_centering);
+        pl.loadParam("x_deadband", m_max_x_error);
+        pl.loadParam("y_deadband", m_max_y_error);
+
+        if (not pl.loadedSuccessfully()) {
+            ROS_ERROR("[GimbalCameraNodelet]: Some compulsory parameters could not be loaded! Ending.");
+            ros::shutdown();
+            return;
+        }
+
 
         // | --------------------- tf transformer --------------------- |
         m_transformer = mrs_lib::Transformer("gimbal_camera");
 
         // | -------------------- initialize timers ------------------- |
 
-//        m_timer_following = nh.createTimer(ros::Duration(0.0001), &GimbalCameraNodelet::follow_apriltag_from_two_vectors,
-//                                           this);
-
         m_timer_centering = nh.createTimer(ros::Duration(m_time_before_centering),
-                                           &GimbalCameraNodelet::center_camera,
-                                           this);
+                                           &GimbalCameraNodelet::center_camera, this);
+        // | ------------ initialize publishers subscribers ------------|
+        m_pub_transform2gimbal_pry = nh.advertise<mrs_msgs::GimbalPRY>("cmd_pry", 1);
 
-        m_pub_transform2gimbal_pry = nh.advertise<mrs_msgs::GimbalPRY>("/uav1/gimbal_driver/cmd_pry", 1);
-
-        m_pub_transform2gimbal_quat = nh.advertise<geometry_msgs::QuaternionStamped>(
-                "/uav1/gimbal_driver/cmd_orientation", 1);
+        m_pub_transform2gimbal_quat = nh.advertise<geometry_msgs::QuaternionStamped>("cmd_quat", 1);
 
         m_sub_tag_detection = nh.subscribe("/tag_detections", 8,
                                            &GimbalCameraNodelet::m_cbk_tag_detection,
@@ -41,8 +47,7 @@ namespace gimbal_camera {
         m_sub_camera_info = nh.subscribe("/camera/camera_info", 8,
                                          &GimbalCameraNodelet::m_cbk_camera_info,
                                          this);
-        ROS_INFO_ONCE("[GimbalCameraNodelet]: initialized");
-
+        ROS_INFO_ONCE("[GimbalCameraNodelet]: Nodelet initialized");
     }
 //}
 
@@ -58,32 +63,26 @@ namespace gimbal_camera {
     }
 
     void GimbalCameraNodelet::m_cbk_tag_detection(const apriltag_ros::AprilTagDetectionArray msg) {
-        std::vector<int> a;
-        if (msg.detections.size() == 0) {
-            return;
-        }
-        auto detection = msg.detections.back();
+        if (msg.detections.size() == 0) return;
+        auto pose = msg.detections.back().pose.pose.pose;
 
-        const auto vec2apriltag = Eigen::Vector3d{detection.pose.pose.pose.position.x,
-                                                  detection.pose.pose.pose.position.y,
-                                                  detection.pose.pose.pose.position.z};
+        const auto vec2apriltag = Eigen::Vector3d{pose.position.x,
+                                                  pose.position.y,
+                                                  pose.position.z};
 
-        Eigen::Quaterniond orientation = Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d::UnitX(), vec2apriltag);
+        Eigen::Quaterniond qt = Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d::UnitX(), vec2apriltag);
 
-        const auto x_error = static_cast<float>(detection.pose.pose.pose.position.x);
-        const auto y_error = static_cast<float>(detection.pose.pose.pose.position.y);
+        const auto x_error = static_cast<float>(pose.position.x);
+        const auto y_error = static_cast<float>(pose.position.y);
 
-        const bool x_change_flag = std::abs(x_error) > m_max_x_error;
-        const bool y_change_flag = std::abs(y_error) > m_max_y_error;
-
-        if (x_change_flag or y_change_flag) {
+        if (std::abs(y_error) > m_max_y_error or std::abs(x_error) > m_max_x_error) {
             auto msg_quat = boost::make_shared<geometry_msgs::QuaternionStamped>();
             msg_quat->header.stamp = ros::Time::now();
             msg_quat->header.frame_id = "uav1/gimbal/camera_optical";
-            msg_quat->quaternion.x = orientation.x();
-            msg_quat->quaternion.y = orientation.y();
-            msg_quat->quaternion.z = orientation.z();
-            msg_quat->quaternion.w = orientation.w();
+            msg_quat->quaternion.x = qt.x();
+            msg_quat->quaternion.y = qt.y();
+            msg_quat->quaternion.z = qt.z();
+            msg_quat->quaternion.w = qt.w();
 
             m_pub_transform2gimbal_quat.publish(msg_quat);
             ROS_INFO_THROTTLE(1.0, "[GimbalCamera]: msg sent");
@@ -94,106 +93,13 @@ namespace gimbal_camera {
     }
 // | --------------------- timer callbacks -------------------- |
 
-
-    [[maybe_unused]] void
-    GimbalCameraNodelet::follow_apriltag_from_two_vectors() {
-        if (not m_recv_camera_info) return;
-        const auto tf_tag_cam = m_transformer.getTransform("mbundle", "camera", ros::Time::now());
-
-        if (!tf_tag_cam.has_value() or (ros::Time::now().sec - tf_tag_cam->stamp().sec > 1)) return;
-
-        const auto transformation = tf_tag_cam->getTransform().transform;
-
-        const auto vec2apriltag = Eigen::Vector3d{transformation.translation.x,
-                                                  transformation.translation.y,
-                                                  transformation.translation.z};
-
-        Eigen::Quaterniond orientation = Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d::UnitX(), vec2apriltag);
-
-        const auto x_error = static_cast<float>(transformation.translation.x);
-        const auto y_error = static_cast<float>(transformation.translation.y);
-
-        const bool x_change_flag = std::abs(x_error) > m_max_x_error;
-        const bool y_change_flag = std::abs(y_error) > m_max_y_error;
-
-        if (x_change_flag or y_change_flag) {
-            auto msg_quat = boost::make_shared<geometry_msgs::QuaternionStamped>();
-            msg_quat->header.stamp = ros::Time::now();
-            msg_quat->header.frame_id = "uav1/gimbal/camera_optical";
-            msg_quat->quaternion.x = orientation.x();
-            msg_quat->quaternion.y = orientation.y();
-            msg_quat->quaternion.z = orientation.z();
-            msg_quat->quaternion.w = orientation.w();
-            m_pub_transform2gimbal_quat.publish(msg_quat);
-            ROS_INFO_THROTTLE(1.0, "[GimbalCamera]: msg sent");
-        } else {
-            ROS_INFO_THROTTLE(1.0, "[GimbalCamera]: no changes - msg is not sent");
-        }
-
-    }
-
-    [[maybe_unused]] void GimbalCameraNodelet::follow_apriltag_incremental([[maybe_unused]] const ros::TimerEvent &ev) {
-        if (not m_recv_camera_info) return;
-        const auto tf_tag_cam = m_transformer.getTransform("mbundle", "camera", ros::Time::now());
-
-        if (!tf_tag_cam.has_value() or (ros::Time::now().sec - tf_tag_cam->stamp().sec > 0.5)) {
-            return;
-        }
-
-        ROS_INFO_THROTTLE(1.0, "[GimbalCamera]: Incremental apriltag following");
-
-        const auto translation = tf_tag_cam->getTransform().transform.translation;
-
-        const auto x_error = static_cast<float>(translation.x);
-        const auto y_error = static_cast<float>(translation.y);
-
-        const bool x_change_flag = std::abs(x_error) > m_max_x_error;
-        const bool y_change_flag = std::abs(y_error) > m_max_y_error;
-
-        const std::lock_guard<std::mutex> lock(m_movement_mutex);
-        if (x_change_flag) {
-            const auto step = calculate_step(x_error, m_max_x_error);
-            if (x_error < 0) {
-                if (m_yaw_movement < m_max_angle) m_yaw_movement += step;
-            } else {
-                if (m_yaw_movement > -m_max_angle) m_yaw_movement -= step;
-            }
-        }
-        if (y_change_flag) {
-            const auto step = calculate_step(y_error, m_max_y_error);
-            if (y_error < 0) {
-                if (m_pitch_movement > -m_max_angle) m_pitch_movement -= step;
-            } else {
-                if (m_pitch_movement < m_max_angle) m_pitch_movement += step;
-            }
-        }
-
-        if (x_change_flag or y_change_flag) {
-            auto msg_pry = boost::make_shared<mrs_msgs::GimbalPRY>();
-            msg_pry->roll = 0;
-            msg_pry->yaw = m_yaw_movement;
-            msg_pry->pitch = m_pitch_movement;
-            m_pub_transform2gimbal_pry.publish(msg_pry);
-
-            ROS_INFO_THROTTLE(1.0, "[GimbalCamera]: msg sent");
-            //ROS_INFO("[GimbalCamera]: %f - x_movement deg, %f - y_movement deg", rad2deg(m_yaw_movement),
-            //         rad2deg(m_pitch_movement));
-            //ROS_INFO("[GimbalCamera]: %f - x_err, %f - y_err", x_error, y_error);
-        } else {
-            ROS_INFO_THROTTLE(1.0, "[GimbalCamera]: no changes - msg is not sent");
-        }
-    }
-
     void GimbalCameraNodelet::center_camera([[maybe_unused]] const ros::TimerEvent &ev) {
         if (not m_recv_camera_info) return;
 
         const auto tf_tag_cam = m_transformer.getTransform("mbundle", "camera", ros::Time::now());
-        const std::lock_guard<std::mutex> lock(m_movement_mutex);
         if (!tf_tag_cam.has_value() or (ros::Time::now().sec - tf_tag_cam->stamp().sec > m_time_before_centering)) {
-            ROS_ERROR(
+            ROS_ERROR_THROTTLE(1.0,
                     "[GimbalCameraNodelet]: Could not transform commanded orientation from frame mbundle to camera, ignoring.");
-            m_yaw_movement = 0.0;
-            m_pitch_movement = 0.0;
             auto m = boost::make_shared<mrs_msgs::GimbalPRY>();
             m->yaw = 0;
             m->pitch = 0;
